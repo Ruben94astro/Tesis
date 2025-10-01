360915603281635618134d7ae740b27ded16144a
Ruben Carcamo <rubencarcamo.01@gmail.com>
2025-10-01 18:39:24 -0300

    Add files via upload

---
+++ cod_parallelize.py
@@ -0,0 +1,402 @@
+import numpy as np
+import pandas as pd
+import astropy.units as u
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import normalize
+import os
+from PIL import Image
+import glob
+import subprocess
+import matplotlib as mpl
+import gc
+import matplotlib
+#import seaborn as sns
+from sklearn.metrics import mean_squared_error
+import emcee
+import corner
+from concurrent.futures import ProcessPoolExecutor
+import tqdm  # opcional, para barra de progreso
+
+
+os.makedirs("frames", exist_ok=True)
+# --- Esto va al inicio del archivo, nivel superior ---
+def animate(i, points, base_intensity, elev, azim,
+                  total_frames, vmin, vmax, spots, r_val, cadence_time):
+            # background configurations
+    plt.style.use('dark_background')
+    fig = plt.figure(figsize=(10, 8))
+    ax_sphere = fig.add_subplot(111, projection='3d') 
+    ax_sphere.set_axis_off()
+    ax_sphere.set_box_aspect([1, 1, 1])
+    ax_sphere.view_init(elev=elev, azim=azim)
+    
+
+    
+    #fig = plt.figure(figsize=(6, 6))
+    #ax_sphere = fig.add_subplot(111, projection='3d')
+    #ax_sphere.set_axis_off()
+    #ax_sphere.set_box_aspect([1, 1, 1])
+    #ax_sphere.view_init(elev=elev, azim=azim)
+
+    intensity = np.copy(base_intensity)
+
+    for spot in spots:
+        theta_mov = spot['theta'] + spot['angular_velocity'] * i * cadence_time.to(u.day)
+        spot_x = r_val * np.sin(spot['phi']) * np.cos(theta_mov.value)
+        spot_y = r_val * np.sin(spot['phi']) * np.sin(theta_mov.value)
+        spot_z = r_val * np.cos(spot['phi'])
+        spot_center = np.array([spot_x, spot_y, spot_z])
+
+        mask = spot_mask_geodesic(points[:,0], points[:,1], points[:,2],
+                                  spot_center, spot['radius'])
+        intensity *= (1 - mask)
+
+    ax_sphere.scatter(points[:,0], points[:,1], points[:,2],
+                      c=np.clip(intensity,0,1), cmap='gray', s=1,
+                      vmin=vmin, vmax=vmax)
+
+
+
+
+    max_range = r_val * 1.1
+    ax_sphere.set_xlim(-max_range, max_range)
+    ax_sphere.set_ylim(-max_range, max_range)
+    ax_sphere.set_zlim(-max_range, max_range)
+
+    os.makedirs("frames", exist_ok=True)
+    plt.savefig(f"frames/frame_{i:05d}.png", dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+    return 
+
+
+# ---- fibonacci sphere ----
+def fibonacci_sphere(n_points):
+    """generating equal points to distributing in sphere"""
+    indices = np.arange(n_points, dtype=float) + 0.5
+    phi = np.arccos(1 - 2*indices/n_points)  # [0, π]
+    theta = np.pi * (1 + 5**0.5) * indices  # aureo angle
+    
+    return phi, theta % (2*np.pi)  # etting values between 0 and 2π
+
+def cartesian_from_spherical(phi, theta, r=1.0):
+    """Convert spherical coordinates to cartesians"""
+    x = r * np.sin(phi) * np.cos(theta)
+    y = r * np.sin(phi) * np.sin(theta)
+    z = r * np.cos(phi)
+    return x, y, z
+
+def spot_mask_geodesic(x, y, z, spot_center, spot_radius_rad):
+    """Máscara usando distancia geodésica real con optimización"""
+    # Convert the spot_center vector to a unit vector (of length 1).
+    center_norm = spot_center / np.linalg.norm(spot_center)
+    
+    # Product
+    positions = np.stack([x, y, z], axis=-1)
+    norms = np.linalg.norm(positions, axis=-1, keepdims=True)
+    pos_norm = positions / np.clip(norms, 1e-10, None)     
+    dot_product = np.sum(pos_norm * center_norm, axis=-1) 
+    dot_product = np.clip(dot_product, -1, 1) 
+    
+    # central_angle is the geodesic distance (in radians) betwe.
+    central_angle = np.arccos(dot_product)
+    
+    return np.exp(-(central_angle**2) / (2 * spot_radius_rad**2))
+
+#function to calculate angular velocity  
+def spot_theta(rotation_period, spot_colatitude, relative_shear):
+    ''' Parameters:
+    - rotation_period: rotation period of the sta
+    - spot_colatitude: latitud of spot(radians)
+    - relative_shear: parameter between pole and equator
+    Returns:
+    - angular velocity. '''
+    
+    latitude = np.pi / 2 - spot_colatitude
+    angular_vel_equa = 2*np.pi*u.rad/rotation_period
+    angular_velocity = angular_vel_equa*(1-relative_shear*np.sin(latitude)**2)
+    return angular_velocity
+
+def limbdarkening(u, mu):
+    ''' lineal limb darkening'''
+    return (1 - u * (1 - mu))
+
+def quadratic(u1, u2, mu):
+    return 1-u1*(1-mu)-u2*(1-mu)**2
+    
+
+def add_spots(latitude_deg, longitude_deg, radii_deg):
+    colatitude_rad = np.deg2rad(90 - latitude_deg)
+    longitude_rad = np.deg2rad(longitude_deg)
+    radii_rad = np.deg2rad(radii_deg)
+    ang_vel = spot_theta(rotation_period, colatitude_rad, 0.4)  
+    spots.append({
+        'theta': longitude_rad * u.rad,
+        'phi': colatitude_rad,
+        'radius': radii_rad,
+        'angular_velocity': ang_vel
+    })
+    #return spots
+def gif(input_pattern="frames/frame_%05d.png", output_gif="output.gif", 
+        palette="palette.png", framerate=17):
+    palette_cmd = [
+        "ffmpeg", "-y", "-i", input_pattern,
+        "-vf", "palettegen", palette
+    ]
+    gif_cmd = [
+        "ffmpeg", "-y", "-framerate", str(framerate),
+        "-i", input_pattern, "-i", palette,
+        "-lavfi", "paletteuse", output_gif
+    ]
+    try:
+        subprocess.run(palette_cmd, check=True)
+        subprocess.run(gif_cmd, check=True)
+        print(f"GIF creado: {output_gif}")
+    except subprocess.CalledProcessError as e:
+        print("Error en ffmpeg:", e)
+
+def flux_plot(theta_vec):
+    '''
+    Function take a list normalizing the flux, converting the list in a 
+csv file and rename the columns
+    and return a plot 
+
+    '''
+    #lat,lon,radii = theta_vec
+    lat, lon, radii = theta_vec
+    frame_files = sorted(glob.glob("frames/frame_*.png"))
+    fluxes = []
+
+    for filename in frame_files:
+        img = Image.open(filename).convert('L')  # Grayscale
+        img_array = np.array(img, dtype=np.float64)
+        flux_total = np.sum(img_array)
+        fluxes.append(flux_total)
+
+    # Normalized fluxes
+    #flux_norm.append(flux_total / fluxes[i])
+    flux_norm = normalize([fluxes], norm="max")[0]
+    #flux_norm = np.array(fluxes)/np.max(np.array(fluxes))
+    df = pd.DataFrame(flux_norm)
+
+    # Creating columns
+    df.index.name = 'Frame'
+    df.reset_index(inplace=True)
+    #changing frames for days
+    df['Days'] = df['Frame'] *(cadence_time.to(u.day)).value
+    df = df.rename(columns={0: 'flux_normalized'})
+    df = df[['Days', 'flux_normalized']]  
+
+    # saving csv
+    df.to_csv(f'lat_{lat}_lon_{lon}_radii{radii}.csv', index=False)
+ 
+    
+    #df.to_csv("test.csv", index=False)
+    # plotting
+    
+    ax = df.plot(x="Days", y="flux_normalized", alpha=0.5, linestyle='--', color ="k")
+    ax.set_xlabel("Time [days]")
+    ax.set_ylabel("Normalized Flux")
+    ax.set_title("Lightcurve from PNG frames")
+    plt.style.use('default')
+    plt.tight_layout()
+    plt.savefig(f'lat_{lat}_lon{lon}_radii{radii}.png', dpi=600)
+    plt.show()
+    return df["flux_normalized"], df["Days"]
+
+# ---- Función de animación corregida ----
+
+
+
+def run_parallel_frames(points, base_intensity, elev, azim,
+                        total_frames, vmin, vmax, spots, r_val, cadence_time,
+                        n_workers=96):
+    with ProcessPoolExecutor(max_workers=n_workers) as executor:
+        futures = [
+            executor.submit(animate_frame, i, points, base_intensity,
+                            elev, azim, total_frames, vmin, vmax,
+                            spots, r_val, cadence_time)
+            for i in range(total_frames)
+
+        ]
+        for f in tqdm.tqdm(futures):
+            f.result()
+
+
+
+ # Generating animation
+def star_animate(theta_vec):
+    
+    #lat,lon,radii = theta_vec
+    lat,lon,radii = theta_vec
+    add_spots(lat, lon, radii)
+        # spherical grid with fibonacci points
+    print("Generate spherical grid")
+    phi, theta = fibonacci_sphere(n_points)
+    x, y, z = cartesian_from_spherical(phi, theta)
+    points = np.vstack([x, y, z]).T
+    
+    # Calculate point of view
+    elev_rad = np.deg2rad(elev) #elevation of point of view
+    azim_rad = np.deg2rad(azim)#azimut of point of view
+    
+    v_x = np.cos(elev_rad) * np.cos(azim_rad)
+    v_y = np.cos(elev_rad) * np.sin(azim_rad)
+    v_z = np.sin(elev_rad)
+    
+    # rearrange of calculating mu parameter for limb darkening
+    mu = (points[:, 0] * v_x + points[:, 1] * v_y + points[:, 2] * v_z) / r_val
+    mu = np.clip(mu, 0, 1)
+    #base_intensity = limbdarkening(constant, mu)# applying to the texture
+    base_intensity = quadratic(u1,u2,mu) 
+        #    Calculates the extreme values of the base intensity: 
+    #vmin: Minimum value of intensity in the whole star.
+    #vmax: Maximum value of intensity over the whole star.
+    vmin=  0.0
+    vmax=  1.0
+
+   # Defines a reference range for color mapping that will be used consistently across all frames.
+            # background configurations
+    plt.style.use('dark_background')
+    fig = plt.figure(figsize=(10, 8))
+    ax_sphere = fig.add_subplot(111, projection='3d') 
+    ax_sphere.set_axis_off()
+    ax_sphere.set_box_aspect([1, 1, 1])
+    
+ 
+    
+
+    print("start render...")
+ # usa el # de núcleos que tengas
+
+        #animate(i, points, base_intensity, ax_sphere,  elev, azim, total_frames, vmin, vmax)
+    # Create gif an light curve
+    #print("\nCreando GIF...")
+    #gif(input_pattern="frames/frame_%05d.png", output_gif=f"period_{rotation_period}_points{n_points}_obs{observing_baseline_days}_cadence{cadence_time}_nspots{len(spots)}.gif", framerate=15)
+    #to verify if is mcmc is working 
+    #print(theta_vec)
+    run_parallel_frames(points, base_intensity, elev, azim,
+                    total_frames, vmin, vmax, spots, r_val, cadence_time,
+                    n_workers=96) 
+    
+    plt.style.use('default')
+    spots.clear()
+    plt.close(fig) 
+ 
+    return flux_plot(theta_vec)
+
+def load_tess():
+# load tess light_curves
+    csv1 = "test.csv"
+    #csv2 = f"lat_{float(vec[0])}_lon{float(vec[1])}_radii{float(vec[2])}.csv"
+    
+    df1 = pd.read_csv(csv1)
+    #df2 = pd.read_csv(csv2)
+    F = df1["flux_normalized"]
+    days =df1["Days"]
+    return F, days 
+
+#  verification function to find the aprox parameters value, recibing the test light curve and days
+def function_mse(flux, days):
+    #simulated folder
+    sim_folder = 'light_curve'
+    results = []
+    #search csv, calculate MSE 
+    for file in os.listdir(sim_folder):
+        if file.endswith('.csv'):
+            path = os.path.join(sim_folder, file)
+            try:
+                sim_curve = pd.read_csv(path)
+                sim_flux = sim_curve['flux_normalized'].values
+                sim_time = sim_curve['Days'].values
+    
+                # if both lenth are equal
+                if len(sim_flux) != len(flux):
+                    print("❌ light curves are differents.")
+                    
+                else:
+            
+        
+                    # MSE mean square error
+                    mse = np.mean((flux-sim_flux)**2)
+        
+                    # Extract parameters
+                    name_parts = file.replace('.csv', '').split('_')
+                    lat = float(name_parts[1].replace('lat', '').replace('-', '-'))
+                    lon = float(name_parts[2].replace('lon', ''))
+                    rad = float(name_parts[3].replace('rad', ''))
+        
+                    results.append({
+                        'file': file,
+                        'lat': lat,
+                        'lon': lon,
+                        'rad': rad,
+                        'mse': mse,
+                        'flux': sim_flux
+                    })
+        
+            except Exception as e:
+                print(f"Error reading {file}: {e}")
+    
+    # sorting top 5
+    top5 = sorted(results, key=lambda x: x['mse'])[:5]
+    
+    print("\nTop 5 likely light curve:\n")
+    for i, r in enumerate(top5, 1):
+        print(f"{i}. Archivo: {r['file']}, Lat: {r['lat']}, Lon: {r['lon']}, Rad: {r['rad']}, MSE: {r['mse']:.10f}")
+    
+    # plotting LC
+    plt.figure(figsize=(12, 8))
+    for i, r in enumerate(top5, 1):
+        plt.subplot(3, 2, i)
+        plt.plot(days, flux, label='lc test', color='black')
+        plt.plot(days, r['flux'], label=f"Simulated {i}", linestyle='--')
+        plt.title(f"{r['file']} (MSE: {r['mse']:.10f})")
+        plt.xlabel("Days")
+        plt.ylabel("Flux normalized")
+        plt.legend()
+    
+    plt.tight_layout()
+    plt.suptitle("Top 5 MSE light curve", fontsize=16, y=1.02)
+    plt.show()
+
+    #return values to be saved in a vector
+    return top5[0]["lat"], top5[0]["lon"], top5[0]["rad"]
+    
+
+
+
+# ---- main ----
+if __name__ == '__main__':
+    # stellar parameter
+    r_val = 1.0
+    n_points = 80000
+    u1 = 0.4
+    u2 =0.3# limb darkening coefficients
+    rotation_period = 1.0 * u.day
+    
+    # Point of view
+    elev = 0
+    azim = 0
+    
+    # List of spots
+    spots = []
+  
+    
+    # base lines time parameter
+    observing_baseline_days = 1 * u.day
+    cadence_time = 2 * u.minute
+    total_frames = int((observing_baseline_days / cadence_time).decompose().value)
+    
+    #load flux and days of test light curve
+    #flux_test, days_test =load_tess()
+    #parameters where we are selecting the top1 simulated lightcurve by MSE
+    #initial_params = function_mse(flux_test, days_test)
+    theta = (20, 0, 15)
+    star_animate(theta)
+
+    
+
+
+
